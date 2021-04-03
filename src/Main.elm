6c71a8da323af1048f5f91d42d5a56ae7321070b
elsanussi-s-mneina <200~52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-04-03 16:21:04 -0500

    Add input text area

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,16 @@
 module Main exposing (main)
 
-import Html exposing (Html, span, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, div, input, span, text)
+import Html.Attributes exposing (class, placeholder, value)
 
+
+inputTextArea : Html msg
+inputTextArea = 
+           input [ placeholder "IPA Numbers", value "Put content here"] []
 
 main : Html a
-main = span [class "welcome-message"] [text "Programmed by Elsanussi Mneina, 2021"]
+main = div []
+       [
+           inputTextArea
+           , span [class "welcome-message"] [text "Programmed by Elsanussi Mneina, 2021"]
+       ]
