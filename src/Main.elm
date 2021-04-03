19b65376e8582e65ace993067b1a1f2e7e8f7c5f
elsanussi-s-mneina <200~52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-04-03 16:12:01 -0500

    Add page with single line of text

---
+++ src/Main.elm
@@ -0,0 +1,8 @@
+module Main exposing (main)
+
+import Html exposing (Html, span, text)
+import Html.Attributes exposing (class)
+
+
+main : Html a
+main = span [class "welcome-message"] [text "Programmed by Elsanussi Mneina, 2021"]
