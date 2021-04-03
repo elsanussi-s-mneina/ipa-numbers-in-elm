52fedd4154a18f6ece8fb8845171eadf034a700d
elsanussi-s-mneina <200~52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-04-03 16:46:14 -0500

    Wire interactivity infrastructure: updating

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,58 @@
 module Main exposing (main)
 
-import Html exposing (Html, div, input, span, text)
+import Browser
+import Html exposing (Html, Attribute, div, input, span, text)
 import Html.Attributes exposing (class, placeholder, value)
+import Html.Events exposing (onInput)
 
+inputTextArea : Model -> Html Msg
+inputTextArea model = 
+           input [ placeholder "IPA Numbers", value model.ipaNumbersContent, onInput ChangeInIPANumbers] []
 
-inputTextArea : Html msg
-inputTextArea = 
-           input [ placeholder "IPA Numbers", value ""] []
+outputTextArea : Model -> Html Msg
+outputTextArea model =
+           input [ placeholder "IPA Characters", value model.ipaCharactersContent, onInput ChangeInIPAChars] []
 
-outputTextArea =
-           input [ placeholder "IPA Characters", value ""] []
+-- MAIN
 
-main : Html a
-main = div []
+main = Browser.sandbox { init = init, update = update, view = view }
+
+-- Model
+
+type alias Model =
+  { ipaNumbersContent : String
+  , ipaCharactersContent : String
+  }
+
+init : Model
+init =
+  { ipaNumbersContent = "", ipaCharactersContent = "" }
+
+-- UPDATE
+type Msg
+  = ChangeInIPAChars String | ChangeInIPANumbers String
+
+ipaNumbersToCharacters : String -> String
+ipaNumbersToCharacters x = x -- To be implemented.
+
+ipaCharactersToNumbers : String -> String
+ipaCharactersToNumbers x = x -- To be implemented.
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    ChangeInIPAChars newContent ->
+            {model | 
+                    ipaCharactersContent = newContent  -- Allows normal input to text field. 
+                    , ipaNumbersContent = ipaCharactersToNumbers newContent }
+    ChangeInIPANumbers newContent ->
+            {model | ipaNumbersContent = newContent -- Allow normal input 
+                   , ipaCharactersContent = ipaNumbersToCharacters newContent }
+
+view : Model -> Html Msg
+view model =  div []
        [
-           inputTextArea
-           , outputTextArea
+           inputTextArea model
+           , outputTextArea model
            , span [class "welcome-message"] [text "Programmed by Elsanussi Mneina, 2021"]
        ]
