7429fee3c8c9258a83ce2f6ccb74ee4428e7914e
elsanussi-s-mneina <200~52800671+elsanussi-s-mneina@users.noreply.github.com>
2021-04-03 17:07:54 -0500

    Add constants in separate modules

---
+++ src/IPAConstants/IPAUnicodeConstants.elm
@@ -0,0 +1,371 @@
+-- | This module contains the definitions of the Unicode code points
+-- for IPA Characters.
+
+-- Note that the name of the identifiers is the name used
+-- in Unicode charts. This is the best way to have consistent names.
+-- Unicode charts may be found at http://unicode.org/charts/ .
+-- Note that all IPA Characters are encoded within the Latin charts.
+module IPAConstants.IPAUnicodeConstants exposing (..)
+
+
+latin_small_letter_p : Char
+latin_small_letter_p = '\u{0070}' -- p
+
+latin_small_letter_b : Char
+latin_small_letter_b = '\u{0062}' -- b
+
+latin_small_letter_t : Char
+latin_small_letter_t = '\u{0074}' -- t
+
+latin_small_letter_d : Char
+latin_small_letter_d = '\u{0064}' -- d
+latin_small_letter_t_with_retroflex_hook : Char
+latin_small_letter_t_with_retroflex_hook = '\u{0288}' -- ʈ
+latin_small_letter_d_with_tail : Char
+latin_small_letter_d_with_tail = '\u{0256}' -- ɖ
+latin_small_letter_c : Char
+latin_small_letter_c = '\u{0063}' -- c
+latin_small_letter_dotless_j_with_stroke : Char
+latin_small_letter_dotless_j_with_stroke = '\u{025F}' -- ɟ
+latin_small_letter_k : Char
+latin_small_letter_k = '\u{006B}' -- k
+latin_small_letter_g : Char
+latin_small_letter_g = '\u{0067}'
+
+latin_small_letter_script_g : Char
+latin_small_letter_script_g = '\u{0261}'
+
+
+latin_small_letter_q : Char
+latin_small_letter_q = '\u{0071}' -- q
+latin_letter_small_capital_g : Char
+latin_letter_small_capital_g = '\u{0262}' -- ɢ
+latin_letter_glottal_stop : Char
+latin_letter_glottal_stop = '\u{0294}' -- ʔ
+latin_small_letter_m : Char
+latin_small_letter_m = '\u{006D}' -- m
+latin_small_letter_m_with_hook : Char
+latin_small_letter_m_with_hook = '\u{0271}' -- ɱ
+latin_small_letter_n : Char
+latin_small_letter_n = '\u{006E}' -- n
+latin_small_letter_n_with_retroflex_hook : Char
+latin_small_letter_n_with_retroflex_hook = '\u{0273}' -- ɳ
+latin_small_letter_n_with_left_hook : Char
+latin_small_letter_n_with_left_hook = '\u{0272}' -- ɲ
+latin_small_letter_eng : Char
+latin_small_letter_eng = '\u{014B}' -- ŋ
+latin_letter_small_capital_n : Char
+latin_letter_small_capital_n = '\u{0274}' -- ɴ
+latin_letter_small_capital_b : Char
+latin_letter_small_capital_b = '\u{0299}' -- ʙ
+latin_small_letter_r : Char
+latin_small_letter_r = '\u{0072}' -- r
+latin_letter_small_capital_r : Char
+latin_letter_small_capital_r = '\u{0280}' -- ʀ
+latin_small_letter_v_with_right_hook : Char
+latin_small_letter_v_with_right_hook = '\u{2C71}' -- ⱱ
+latin_small_letter_r_with_fishhook : Char
+latin_small_letter_r_with_fishhook = '\u{027E}' -- ɾ
+latin_small_letter_r_with_tail : Char
+latin_small_letter_r_with_tail = '\u{027D}' -- ɽ
+latin_small_letter_phi : Char
+latin_small_letter_phi = '\u{0278}' -- ɸ
+greek_small_letter_beta : Char
+greek_small_letter_beta = '\u{03B2}' -- β
+latin_small_letter_f : Char
+latin_small_letter_f = '\u{0066}' -- f
+latin_small_letter_v : Char
+latin_small_letter_v = '\u{0076}' -- v
+greek_small_letter_theta : Char
+greek_small_letter_theta = '\u{03B8}' -- θ
+latin_small_letter_eth : Char
+latin_small_letter_eth = '\u{00F0}' -- ð
+latin_small_letter_s : Char
+latin_small_letter_s = '\u{0073}' -- s
+latin_small_letter_z : Char
+latin_small_letter_z = '\u{007A}' -- z
+latin_small_letter_esh : Char
+latin_small_letter_esh = '\u{0283}' -- ʃ
+latin_small_letter_ezh : Char
+latin_small_letter_ezh = '\u{0292}' -- ʒ
+latin_small_letter_s_with_hook : Char
+latin_small_letter_s_with_hook = '\u{0282}' -- ʂ
+latin_small_letter_z_with_retroflex_hook : Char
+latin_small_letter_z_with_retroflex_hook = '\u{0290}' -- ʐ
+latin_small_letter_c_with_cedilla : Char
+latin_small_letter_c_with_cedilla = '\u{00E7}' -- ç
+latin_small_letter_j_with_crossed_tail : Char
+latin_small_letter_j_with_crossed_tail = '\u{029D}' -- ʝ
+latin_small_letter_x : Char
+latin_small_letter_x = '\u{0078}' -- x
+latin_small_letter_gamma : Char
+latin_small_letter_gamma = '\u{0263}' -- ɣ
+greek_small_letter_chi : Char
+greek_small_letter_chi = '\u{03C7}' -- χ
+latin_letter_small_capital_inverted_r : Char
+latin_letter_small_capital_inverted_r = '\u{0281}' -- ʁ
+latin_small_letter_h_with_stroke : Char
+latin_small_letter_h_with_stroke = '\u{0127}' -- ħ
+latin_letter_pharyngeal_voiced_fricative : Char
+latin_letter_pharyngeal_voiced_fricative = '\u{0295}' -- ʕ
+latin_small_letter_h : Char
+latin_small_letter_h = '\u{0068}' -- h
+latin_small_letter_h_with_hook : Char
+latin_small_letter_h_with_hook = '\u{0266}' -- ɦ
+latin_small_letter_l_with_belt : Char
+latin_small_letter_l_with_belt = '\u{026C}' -- ɬ
+latin_small_letter_lezh : Char
+latin_small_letter_lezh = '\u{026E}' -- ɮ
+latin_small_letter_v_with_hook : Char
+latin_small_letter_v_with_hook = '\u{028B}' -- ʋ
+latin_small_letter_turned_r : Char
+latin_small_letter_turned_r = '\u{0279}' -- ɹ
+latin_small_letter_turned_r_with_hook : Char
+latin_small_letter_turned_r_with_hook = '\u{027B}' -- ɻ
+latin_small_letter_j : Char
+latin_small_letter_j = '\u{006A}' -- j
+latin_small_letter_turned_m_with_long_leg : Char
+latin_small_letter_turned_m_with_long_leg = '\u{0270}' -- ɰ
+latin_small_letter_l : Char
+latin_small_letter_l = '\u{006C}' -- l
+latin_small_letter_l_with_retroflex_hook : Char
+latin_small_letter_l_with_retroflex_hook = '\u{026D}' -- ɭ
+latin_small_letter_turned_y : Char
+latin_small_letter_turned_y = '\u{028E}' -- ʎ
+latin_letter_small_capital_l : Char
+latin_letter_small_capital_l = '\u{029F}' -- ʟ
+latin_letter_bilabial_click : Char
+latin_letter_bilabial_click = '\u{0298}' -- ʘ
+latin_letter_dental_click : Char
+latin_letter_dental_click = '\u{01C0}' -- ǀ
+latin_letter_retroflex_click : Char
+latin_letter_retroflex_click = '\u{01C3}' -- ǃ
+latin_letter_alveolar_click : Char
+latin_letter_alveolar_click = '\u{01C2}' -- ǂ
+latin_letter_lateral_click : Char
+latin_letter_lateral_click = '\u{01C1}' -- ǁ
+latin_small_letter_b_with_hook : Char
+latin_small_letter_b_with_hook = '\u{0253}' -- ɓ
+latin_small_letter_d_with_hook : Char
+latin_small_letter_d_with_hook = '\u{0257}' -- ɗ
+latin_small_letter_dotless_j_with_stroke_and_hook : Char
+latin_small_letter_dotless_j_with_stroke_and_hook = '\u{0284}' -- ʄ
+latin_small_letter_g_with_hook : Char
+latin_small_letter_g_with_hook = '\u{0260}' -- ɠ
+latin_letter_small_capital_g_with_hook : Char
+latin_letter_small_capital_g_with_hook = '\u{029B}' -- ʛ
+modifier_letter_apostrophe : Char
+modifier_letter_apostrophe = '\u{02BC}' -- ʼ
+latin_small_letter_i : Char
+latin_small_letter_i = '\u{0069}' -- i
+latin_small_letter_y : Char
+latin_small_letter_y = '\u{0079}' -- y
+latin_small_letter_i_with_stroke : Char
+latin_small_letter_i_with_stroke = '\u{0268}' -- ɨ
+latin_small_letter_u_bar : Char
+latin_small_letter_u_bar = '\u{0289}' -- ʉ
+latin_small_letter_turned_m : Char
+latin_small_letter_turned_m = '\u{026F}' -- ɯ
+latin_small_letter_u : Char
+latin_small_letter_u = '\u{0075}' -- u
+latin_letter_small_capital_i : Char
+latin_letter_small_capital_i = '\u{026A}' -- ɪ
+latin_letter_small_capital_y : Char
+latin_letter_small_capital_y = '\u{028F}' -- ʏ
+latin_small_letter_upsilon : Char
+latin_small_letter_upsilon = '\u{028A}' -- ʊ
+latin_small_letter_e : Char
+latin_small_letter_e = '\u{0065}' -- e
+latin_small_letter_o_with_stroke : Char
+latin_small_letter_o_with_stroke = '\u{00F8}' -- ø
+latin_small_letter_reversed_e : Char
+latin_small_letter_reversed_e = '\u{0258}' -- ɘ
+latin_small_letter_barred_o : Char
+latin_small_letter_barred_o = '\u{0275}' -- ɵ
+latin_small_letter_rams_horn : Char
+latin_small_letter_rams_horn = '\u{0264}' -- ɤ
+latin_small_letter_o : Char
+latin_small_letter_o = '\u{006F}' -- o
+latin_small_letter_schwa : Char
+latin_small_letter_schwa = '\u{0259}' -- ə
+latin_small_letter_open_e : Char
+latin_small_letter_open_e = '\u{025B}' -- ɛ
+latin_small_ligature_oe : Char
+latin_small_ligature_oe = '\u{0153}' -- œ
+latin_small_letter_reversed_open_e : Char
+latin_small_letter_reversed_open_e = '\u{025C}' -- ɜ
+latin_small_letter_closed_reversed_open_e : Char
+latin_small_letter_closed_reversed_open_e = '\u{025E}' -- ɞ
+latin_small_letter_turned_v : Char
+latin_small_letter_turned_v = '\u{028C}' -- ʌ
+latin_small_letter_open_o : Char
+latin_small_letter_open_o = '\u{0254}' -- ɔ
+latin_small_letter_ae : Char
+latin_small_letter_ae = '\u{00E6}' -- æ
+latin_small_letter_turned_a : Char
+latin_small_letter_turned_a = '\u{0250}' -- ɐ
+
+latin_small_letter_a : Char
+latin_small_letter_a = '\u{0061}' --a
+latin_letter_small_capital_oe : Char
+latin_letter_small_capital_oe = '\u{0276}' -- ɶ
+latin_small_letter_alpha : Char
+latin_small_letter_alpha = '\u{0251}' -- ɑ
+latin_small_letter_turned_alpha : Char
+latin_small_letter_turned_alpha = '\u{0252}' -- ɒ
+latin_small_letter_turned_w : Char
+latin_small_letter_turned_w = '\u{028D}' -- ʍ
+latin_small_letter_w : Char
+latin_small_letter_w = '\u{0077}' -- w
+latin_small_letter_turned_h : Char
+latin_small_letter_turned_h = '\u{0265}' -- ɥ
+latin_letter_small_capital_h : Char
+latin_letter_small_capital_h = '\u{029C}' -- ʜ
+latin_letter_reversed_glottal_stop_with_stroke : Char
+latin_letter_reversed_glottal_stop_with_stroke = '\u{02A2}' -- ʢ
+latin_letter_glottal_stop_with_stroke : Char
+latin_letter_glottal_stop_with_stroke = '\u{02A1}' -- ʡ
+latin_small_letter_c_with_curl : Char
+latin_small_letter_c_with_curl = '\u{0255}' -- ɕ
+latin_small_letter_z_with_curl : Char
+latin_small_letter_z_with_curl = '\u{0291}' -- ʑ
+latin_small_letter_turned_r_with_long_leg : Char
+latin_small_letter_turned_r_with_long_leg = '\u{027A}' -- ɺ
+latin_small_letter_heng_with_hook : Char
+latin_small_letter_heng_with_hook = '\u{0267}' -- ɧ
+
+combining_double_inverted_breve : Char
+combining_double_inverted_breve = '\u{0361}' -- k͡p 
+combining_double_breve_below : Char
+combining_double_breve_below = '\u{035C}' -- t͜s  ͡    ͜
+
+modifier_letter_vertical_line : Char
+modifier_letter_vertical_line = '\u{02C8}' -- ˈ
+modifier_letter_low_vertical_line : Char
+modifier_letter_low_vertical_line = '\u{02CC}' -- ˌ
+modifier_letter_triangular_colon : Char
+modifier_letter_triangular_colon = '\u{02D0}' -- ː
+modifier_letter_half_triangular_colon : Char
+modifier_letter_half_triangular_colon = '\u{02D1}' -- ˑ
+combining_breve : Char
+combining_breve = '\u{0306}' -- ĕ
+vertical_line : Char
+vertical_line = '\u{007C}' --   |
+double_vertical_line : Char
+double_vertical_line = '\u{2016}' -- ‖
+full_stop : Char
+full_stop = '\u{002E}' -- .
+undertie : Char
+undertie = '\u{203F}' -- ‿
+combining_ring_below : Char
+combining_ring_below = '\u{0325}' -- ̥
+combining_ring_above : Char
+combining_ring_above = '\u{030A}' -- ̊
+
+combining_caron_below : Char
+combining_caron_below = '\u{032C}' --  ̬
+modifier_letter_small_h : Char
+modifier_letter_small_h = '\u{02B0}' -- ʰ
+combining_right_half_ring_below : Char
+combining_right_half_ring_below = '\u{0339}' -- ̹
+combining_left_half_ring_below : Char
+combining_left_half_ring_below = '\u{031C}' -- ̜
+combining_plus_sign_below : Char
+combining_plus_sign_below = '\u{031F}' -- ̟
+combining_minus_sign_below : Char
+combining_minus_sign_below = '\u{0320}' -- ̠
+combining_diaeresis : Char
+combining_diaeresis = '\u{0308}' -- ̈
+combining_x_above : Char
+combining_x_above = '\u{033D}' -- ̽
+combining_vertical_line_above : Char
+combining_vertical_line_above = '\u{030D}' -- ̍ 
+combining_vertical_line_below : Char
+combining_vertical_line_below = '\u{0329}' -- ̩
+combining_dot_above : Char
+combining_dot_above = '\u{0307}' -- ̇
+combining_dot_below : Char
+combining_dot_below = '\u{0323}' --̣
+
+combining_inverted_breve_below : Char
+combining_inverted_breve_below = '\u{032F}' -- ̯
+modifier_letter_rhotic_hook : Char
+modifier_letter_rhotic_hook = '\u{02DE}' -- ˞
+combining_diaeresis_below : Char
+combining_diaeresis_below = '\u{0324}' -- ̤
+combining_tilde_below : Char
+combining_tilde_below = '\u{0330}' -- ̰
+combining_seagul_below : Char
+combining_seagul_below = '\u{033C}' -- ̼
+modifier_letter_small_w : Char
+modifier_letter_small_w = '\u{02B7}' -- ʷ
+modifier_letter_small_j : Char
+modifier_letter_small_j = '\u{02B2}' -- ʲ
+modifier_letter_small_gamma : Char
+modifier_letter_small_gamma = '\u{02E0}' -- ˠ
+modifier_letter_small_reversed_glottal_stop : Char
+modifier_letter_small_reversed_glottal_stop = '\u{02E4}' -- ˤ
+combining_tilde_overlay : Char
+combining_tilde_overlay = '\u{0334}' --  ̴
+combining_up_tack_below : Char
+combining_up_tack_below = '\u{031D}' -- ̝
+combining_down_tack_below : Char
+combining_down_tack_below = '\u{031E}' -- ̞
+combining_left_tack_below : Char
+combining_left_tack_below = '\u{0318}' -- ̘
+combining_right_tack_below : Char
+combining_right_tack_below = '\u{0319}' -- ̙
+combining_bridge_below : Char
+combining_bridge_below = '\u{032A}' -- ̪
+combining_inverted_bridge_below : Char
+combining_inverted_bridge_below = '\u{033A}' -- ̺
+combining_square_below : Char
+combining_square_below = '\u{033B}' -- ̻
+combining_tilde : Char
+combining_tilde = '\u{0303}' -- ̃
+superscript_latin_small_letter_n : Char
+superscript_latin_small_letter_n = '\u{207F}' -- ⁿ
+modifier_letter_small_l : Char
+modifier_letter_small_l = '\u{02E1}' -- ˡ
+combining_left_angle_above : Char
+combining_left_angle_above = '\u{031A}' -- ̚
+combining_double_acute_accent : Char
+combining_double_acute_accent = '\u{030B}' -- ̋
+combining_acute_accent : Char
+combining_acute_accent = '\u{0301}' -- ́
+combining_macron : Char
+combining_macron = '\u{0304}' -- ̄
+combining_grave_accent : Char
+combining_grave_accent = '\u{0300}' -- ̀
+combining_double_grave_accent : Char
+combining_double_grave_accent = '\u{030F}' -- ̏
+combining_caron : Char
+combining_caron = '\u{030C}' -- ̌
+combining_circumflex_accent : Char
+combining_circumflex_accent = '\u{0302}' -- ̂
+combining_macron_acute : Char
+combining_macron_acute = '\u{1DC4}' -- ᷄
+combining_grave_macron : Char
+combining_grave_macron = '\u{1DC5}' -- ᷅
+combining_grave_acute_grave : Char
+combining_grave_acute_grave = '\u{1DC8}' -- ᷈
+north_east_arrow : Char
+north_east_arrow = '\u{2197}' -- ↗
+south_east_arrow : Char
+south_east_arrow = '\u{2198}' -- ↘
+downwards_arrow : Char
+downwards_arrow = '\u{2193}' -- ↓
+upwards_arrow : Char
+upwards_arrow = '\u{2191}' -- ↑
+modifier_letter_extra_high_tone_bar : Char
+modifier_letter_extra_high_tone_bar = '\u{02E5}' -- ˥
+modifier_letter_high_tone_bar : Char
+modifier_letter_high_tone_bar = '\u{02E6}' -- ˦
+modifier_letter_mid_tone_bar : Char
+modifier_letter_mid_tone_bar = '\u{02E7}' -- ˧
+modifier_letter_low_tone_bar : Char
+modifier_letter_low_tone_bar = '\u{02E8}' -- ˨
+modifier_letter_extra_low_tone_bar : Char
+modifier_letter_extra_low_tone_bar = '\u{02E9}' -- ˩
+
+
